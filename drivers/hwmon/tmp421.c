16864e08d22c53a47875ede00f32cc9750717fc5
Andy Kennedy <Andy.Kennedy@AdTran.com>
2015-11-23 17:24:55 -0600

    I2C board tmp421

--- drivers/hwmon/tmp421.c
+++ drivers/hwmon/tmp421.c
@@ -21,7 +21,7 @@
 
 /*
  * Driver for the Texas Instruments TMP421 SMBus temperature sensor IC.
- * Supported models: TMP421, TMP422, TMP423
+ * Supported models: TMP421, TMP422, TMP423, TMP441, TMP442
  */
 
 #include <linux/module.h>
@@ -36,35 +36,40 @@
 #include <linux/sysfs.h>
 
 /* Addresses to scan */
-static unsigned short normal_i2c[] = { 0x2a, 0x4c, 0x4d, 0x4e, 0x4f,
+static const unsigned short normal_i2c[] = { 0x2a, 0x4c, 0x4d, 0x4e, 0x4f,
 				       I2C_CLIENT_END };
 
 /* Insmod parameters */
-I2C_CLIENT_INSMOD_3(tmp421, tmp422, tmp423);
+I2C_CLIENT_INSMOD_5(tmp421, tmp422, tmp423, tmp441, tmp442);
 
 /* The TMP421 registers */
-#define TMP421_CONFIG_REG_1			0x09
-#define TMP421_CONVERSION_RATE_REG		0x0B
-#define TMP421_MANUFACTURER_ID_REG		0xFE
-#define TMP421_DEVICE_ID_REG			0xFF
+#define TMP421_STATUS_REG			  0x08
+#define TMP421_CONFIG_REG_1			  0x09
+#define TMP421_CONVERSION_RATE_REG	  0x0B
+#define TMP421_MANUFACTURER_ID_REG	  0xFE
+#define TMP421_DEVICE_ID_REG          0xFF
 
 static const u8 TMP421_TEMP_MSB[4]		= { 0x00, 0x01, 0x02, 0x03 };
 static const u8 TMP421_TEMP_LSB[4]		= { 0x10, 0x11, 0x12, 0x13 };
 
 /* Flags */
-#define TMP421_CONFIG_SHUTDOWN			0x40
-#define TMP421_CONFIG_RANGE			0x04
+#define TMP421_CONFIG_SHUTDOWN      0x40
+#define TMP421_CONFIG_RANGE         0x04
 
 /* Manufacturer / Device ID's */
-#define TMP421_MANUFACTURER_ID			0x55
-#define TMP421_DEVICE_ID			0x21
-#define TMP422_DEVICE_ID			0x22
-#define TMP423_DEVICE_ID			0x23
+#define TMP421_MANUFACTURER_ID      0x55
+#define TMP421_DEVICE_ID            0x21
+#define TMP422_DEVICE_ID            0x22
+#define TMP423_DEVICE_ID            0x23
+#define TMP441_DEVICE_ID            0x41
+#define TMP442_DEVICE_ID            0x42
 
 static const struct i2c_device_id tmp421_id[] = {
 	{ "tmp421", tmp421 },
 	{ "tmp422", tmp422 },
 	{ "tmp423", tmp423 },
+	{ "tmp441", tmp441 },
+	{ "tmp442", tmp442 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, tmp421_id);
@@ -227,7 +232,7 @@ static int tmp421_detect(struct i2c_client *client, int kind,
 			 struct i2c_board_info *info)
 {
 	struct i2c_adapter *adapter = client->adapter;
-	const char *names[] = { "TMP421", "TMP422", "TMP423" };
+	const char *names[] = { "TMP421", "TMP422", "TMP423", "TMP441", "TMP442"};
 
 	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE_DATA))
 		return -ENODEV;
@@ -252,6 +257,12 @@ static int tmp421_detect(struct i2c_client *client, int kind,
 		case TMP423_DEVICE_ID:
 			kind = tmp423;
 			break;
+        case TMP441_DEVICE_ID:
+            kind = tmp441;
+            break;
+        case TMP442_DEVICE_ID:
+            kind = tmp442;
+            break;
 		default:
 			return -ENODEV;
 		}
@@ -345,3 +356,5 @@ MODULE_LICENSE("GPL");
 
 module_init(tmp421_init);
 module_exit(tmp421_exit);
+
+
